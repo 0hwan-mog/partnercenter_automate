73c86d858de3cff62566eb5b3fe06db491013c5a
0hwan-mog <kyhwan91@gmail.com>
2022-05-29 23:05:41 +0900

    로그인 정보를 입력하도록 수정

--- partner_center.py
+++ partner_center.py
@@ -40,8 +40,12 @@ def changing_first_order(input_number):
         confirm.click()
         print("주문번호 ", first_order_number,"을(를) 배송중 처리했습니다.")
 
-
+#배송중 처리할 주문건 수 입력
 number_of_order = int(input("몇건의 주문건을 배송중 처리할까요?"))
+
+#파트너센터 로그인 정보 입력
+partnercenter_id = input("파트너센터 테스트계정을 입력하세요")
+partnercenter_password = input("파테스트계정의 비밀번호를 입력하세요")
 
 # 크롬 브라우저 실행후 씨리얼핏 알파 로그인페이지 접속
 driver = webdriver.Chrome()
@@ -52,12 +56,12 @@ driver.get("https://shop.alpha.zigzag.kr/#!/login")
 id = driver.find_element(By.CSS_SELECTOR,
                          "#app > div > main > div > section > form > div.css-1lvmktx.e1yzhszp5 > input")
 id.clear()
-id.send_keys("test@email.com")
+id.send_keys(partnercenter_id)
 
 password = driver.find_element(By.CSS_SELECTOR,
                                "#app > div > main > div > section > form > div.css-1bmzcds.e1yzhszp5 > input[type=password]")
 password.clear()
-password.send_keys("1234")
+password.send_keys(partnercenter_password)
 password.send_keys(Keys.RETURN)
 
 # 쇼핑몰 목록에서 씨리얼핏 클릭
