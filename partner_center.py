71a271d2e32cb25f56e27387ee4d0db2ae9f8d1e
0hwan-mog <kyhwan91@gmail.com>
2022-05-31 15:32:03 +0900

    주석 메시지 정리

--- partner_center.py
+++ partner_center.py
@@ -6,19 +6,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 
-
-
 def changing_first_order(input_number):
     for i in range(0, input_number):
-        # 첫번째 상품 선택 후 송장번호 입력
+        # 첫번째 상품 선택
         first_order = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
                                                                                       "#MainFrame > div.BjciI0DwH9nqgG3nq4qb > div.PqoW0UJNFISUjXDn9nGX.false > div.react-in-mithril > div > div > div:nth-child(3) > div.IuQkXH8WhWwfXxZYXfA7 > div.v90TAbI9_qqLWKbM5Bes > div.W33T7liyyVLq6KNZolFE > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > label > span")))
         first_order.click()
 
+        #택배사 입력
         delivery_company = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
                                                                                            "#MainFrame > div.BjciI0DwH9nqgG3nq4qb > div.PqoW0UJNFISUjXDn9nGX.false > div.react-in-mithril > div > div > div:nth-child(3) > div.IuQkXH8WhWwfXxZYXfA7 > div.v90TAbI9_qqLWKbM5Bes > div.W33T7liyyVLq6KNZolFE > div > table > tbody > tr:nth-child(1) > td:nth-child(8) > select > option:nth-child(54)")))
         delivery_company.click()
 
+
+        #송장번호 입력
         invoice_number = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
                                                                                          "#MainFrame > div.BjciI0DwH9nqgG3nq4qb > div.PqoW0UJNFISUjXDn9nGX.false > div.react-in-mithril > div > div > div:nth-child(3) > div.IuQkXH8WhWwfXxZYXfA7 > div.v90TAbI9_qqLWKbM5Bes > div.W33T7liyyVLq6KNZolFE > div > table > tbody > tr:nth-child(1) > td:nth-child(9) > input")))
         invoice_number.send_keys("123")
@@ -103,7 +104,7 @@ searching.click()
 # 배송중 처리
 try:
     changing_first_order(number_of_order)
-    print("알파 씨리얼핏에 ", client_name, " 님이 1개월 이내 생성한 결제완료 주문건들중 최신순으로", number_of_order, "건을 '배송중' 상태로 변경했습니다.")
+    print("알파 씨리얼핏에", client_name, "님이 1개월 이내 생성한 결제완료 주문건들중 최신순으로", number_of_order, "건을 '배송중' 상태로 변경했습니다.")
 
 except:
     print("오류가 발생했습니다.")
